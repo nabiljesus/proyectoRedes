b3f37e22822cffd009d472c0e79712e970b92cf0
Javier <javierloplom@gmail.com>
2016-06-23 11:01:31 -0400

    Flujo principal del servidor furula

--- proyecto2/server.c
+++ proyecto2/server.c
@@ -17,25 +17,59 @@
 #define HOUR_PRICE 80
 #define HOUR_FRACTION_PRICE 30
 
+#define WENT_IN  1
+#define WENT_OUT 0
+
 /* Ticket structure*/
 struct ticket
 {
     int       identifier; /* Unique ticket/car identifier */
-    struct tm enter_time; /* Time when car checked-in     */
+    struct tm *enter_time; /* Time when car checked-in     */
 };
- 
-FILE *entranceLog, *exitLog;
+
+void update_ticket(struct ticket *t,int newval){
+    time_t time_data;
+    time(&time_data);
+
+    t->identifier = newval;
+    t->enter_time = localtime(&time_data);
+}
+
+
+void write_action(char *output_file,int inside,int door,struct ticket t){
+    char action[8];
+    char time_buffer[30];
+    FILE *log_file = fopen(output_file, "a");
+
+    if (inside) strcpy(action,"entrado");
+    else        strcpy(action,"salido");
+
+    strftime(time_buffer, 30, "%d/%m/%Y %H:%M:%S", t.enter_time);
+
+    fprintf(log_file
+           ,"%s | carro %d ha %s por la puerta %d \n"
+           ,time_buffer
+           ,t.identifier
+           ,action
+           ,door);
+
+    fclose(log_file); 
+}
+
+
 
 int main(int argc, char *argv[])
 {
     int ticket_number     = 0;
     int free_parking_lots = PARKING_LOT_SIZE;
     int listen_port; // Port we will be listening
+    char *entrance_log,*exit_log;
     
     int status;      // Auxiliary to check procedure returning values
 
     int sockfd;
     struct sockaddr_in server_address;
+    struct ticket last_ticket;
 
 
     if (argc != 7) {
@@ -52,17 +86,17 @@ int main(int argc, char *argv[])
         exit(0);
     }
 
-    if      (strcmp("-i",argv[1]) == 0 ) entranceLog = fopen(argv[2],"w");
-    else if (strcmp("-i",argv[3]) == 0 ) entranceLog = fopen(argv[4],"w");
-    else if (strcmp("-i",argv[5]) == 0 ) entranceLog = fopen(argv[6],"w");
+    if      (strcmp("-i",argv[1]) == 0 ) entrance_log = argv[2];
+    else if (strcmp("-i",argv[3]) == 0 ) entrance_log = argv[4];
+    else if (strcmp("-i",argv[5]) == 0 ) entrance_log = argv[6];
     else {
         perror("Error: No se ha especificado la bitacora de entrada");
         exit(0);
     }
 
-    if      (strcmp("-o",argv[1]) == 0 ) exitLog = fopen(argv[2],"w");
-    else if (strcmp("-o",argv[3]) == 0 ) exitLog = fopen(argv[4],"w");
-    else if (strcmp("-o",argv[5]) == 0 ) exitLog = fopen(argv[6],"w");
+    if      (strcmp("-o",argv[1]) == 0 ) exit_log = argv[2];
+    else if (strcmp("-o",argv[3]) == 0 ) exit_log = argv[4];
+    else if (strcmp("-o",argv[5]) == 0 ) exit_log = argv[6];
     else {
         perror("Error: No se ha especificado la bitacora de salida");
         exit(0);
@@ -70,22 +104,58 @@ int main(int argc, char *argv[])
 
 
     /* Server initialization */
-    server_address.sin_family      = AF_INET;            
-    server_address.sin_port        = htons(listen_port); 
-    server_address.sin_addr.s_addr = INADDR_ANY;         
-    bzero(&(server_address.sin_zero), 8); 
+    // server_address.sin_family      = AF_INET;            
+    // server_address.sin_port        = htons(listen_port); 
+    // server_address.sin_addr.s_addr = INADDR_ANY;         
+    // bzero(&(server_address.sin_zero), 8); 
 
-    status = bind(sockfd, (struct sockaddr *)&server_address, sizeof(struct sockaddr));
+    // sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    // if ((sockfd) == -1) {
+    //     perror("socket");
+    //     exit(1);
+    // }
 
-    if (status == -1) {
-        perror("Error: no se pudo realizar el bind");
-        exit(2);
-    }
+    // status = bind(sockfd, (struct sockaddr *)&server_address, sizeof(struct sockaddr));
+
+    // if (status == -1) {
+    //     perror("Error");
+    //     exit(2);
+    // }
 
     // Estados
     while (1){
-        while(free_parking_lots > 0 ) ; // Entregar tickets y aceptar tickets de salida
-        while(free_parking_lots == 0) ; // Rebotar gente hasta encontrar algun ticket de salida
+        // Entregar tickets y aceptar tickets de salida
+        while(free_parking_lots > 0 ) {
+
+
+            // Si alguien entra
+            if (1)
+            {   
+                update_ticket(&last_ticket,ticket_number);
+                write_action(entrance_log,WENT_IN,1,last_ticket);
+
+                ++ticket_number;
+                --free_parking_lots;
+            }
+            else
+            {
+                ++free_parking_lots;
+                // Se debe crear un ticket con los datos recibdos
+                write_action(exit_log,WENT_OUT,1,last_ticket);
+                
+            }
+
+            
+        }
+        sleep(3);
+        // Rebotar gente hasta encontrar algun ticket de salida
+        while(free_parking_lots == 0)
+        {
+            ++free_parking_lots;
+            // Se debe crear un ticket con los datos recibdos
+            write_action(exit_log,WENT_OUT,1,last_ticket);
+        }
+        sleep(3);
     }
 
 }
