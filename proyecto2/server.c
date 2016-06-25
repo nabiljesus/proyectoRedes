749ae59bbefd1cad0b735ef359c5a81a9b591251
Javier <javierloplom@gmail.com>
2016-06-24 23:35:40 -0400

    Agregado socket a server

--- proyecto2/server.c
+++ proyecto2/server.c
@@ -20,10 +20,12 @@
 #define WENT_IN  1
 #define WENT_OUT 0
 
+struct tm * parking_space[PARKING_LOT_SIZE];
+
 /* Ticket structure*/
 struct ticket
 {
-    int       identifier; /* Unique ticket/car identifier */
+    int       identifier;  /* Unique ticket/car identifier */
     struct tm *enter_time; /* Time when car checked-in     */
 };
 
@@ -56,6 +58,28 @@ void write_action(char *output_file,int inside,int door,struct ticket t){
     fclose(log_file); 
 }
 
+void read_message(int socket){
+    int addr_len, numbytes;
+    struct sockaddr_in their_addr;
+    char buf[BUFFER_LEN];
+
+    addr_len = sizeof(struct sockaddr);
+    printf("Esperando datos ....\n");
+    numbytes = recvfrom(socket, buf, 
+                        BUFFER_LEN, 0, 
+                        (struct sockaddr *)&their_addr,
+                        (socklen_t *)&addr_len);
+
+    if ( numbytes == -1 ) {
+        perror("recvfrom");
+        exit(3);
+    }
+
+    printf("paquete proveniente de : %s\n",inet_ntoa(their_addr.sin_addr));
+    printf("longitud del paquete en bytes: %d\n",numbytes);
+    buf[numbytes] = '\0';
+    printf("el paquete contiene: %s\n", buf);
+}
 
 
 int main(int argc, char *argv[])
@@ -68,6 +92,8 @@ int main(int argc, char *argv[])
     int status;      // Auxiliary to check procedure returning values
 
     int sockfd;
+    
+    
     struct sockaddr_in server_address;
     struct ticket last_ticket;
 
@@ -101,33 +127,37 @@ int main(int argc, char *argv[])
         perror("Error: No se ha especificado la bitacora de salida");
         exit(0);
     }
+    /* Initialize parking lot */
+    for (int i = 0; i < PARKING_LOT_SIZE; ++i)
+        parking_space[i] = NULL;
 
 
     /* Server initialization */
-    // server_address.sin_family      = AF_INET;            
-    // server_address.sin_port        = htons(listen_port); 
-    // server_address.sin_addr.s_addr = INADDR_ANY;         
-    // bzero(&(server_address.sin_zero), 8); 
+    server_address.sin_family      = AF_INET;            
+    server_address.sin_port        = htons(listen_port); 
+    server_address.sin_addr.s_addr = INADDR_ANY;         
+    bzero(&(server_address.sin_zero), 8); 
 
-    // sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    // if ((sockfd) == -1) {
-    //     perror("socket");
-    //     exit(1);
-    // }
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
 
-    // status = bind(sockfd, (struct sockaddr *)&server_address, sizeof(struct sockaddr));
+    if (sockfd == -1) { perror("socket"); exit(1); }
 
-    // if (status == -1) {
-    //     perror("Error");
-    //     exit(2);
-    // }
+    status = bind(sockfd
+                 ,(struct sockaddr *)&server_address
+                 ,sizeof(struct sockaddr));
+
+    if (status == -1)  { perror("bind"); exit(2); }
+
+
+    
+
 
     // Estados
     while (1){
         // Entregar tickets y aceptar tickets de salida
         while(free_parking_lots > 0 ) {
 
-
+            read_message(sockfd);
             // Si alguien entra
             if (1)
             {   
@@ -147,10 +177,12 @@ int main(int argc, char *argv[])
 
             
         }
-        sleep(3);
+        
         // Rebotar gente hasta encontrar algun ticket de salida
         while(free_parking_lots == 0)
         {
+            read_message(sockfd);
+
             ++free_parking_lots;
             // Se debe crear un ticket con los datos recibdos
             write_action(exit_log,WENT_OUT,1,last_ticket);
@@ -159,56 +191,3 @@ int main(int argc, char *argv[])
     }
 
 }
-
-// Taller funcional, esto es todo el main
-//     int sockfd; /* descriptor para el socket */
-//     struct sockaddr_in my_addr; /* direccion IP y numero de puerto local */ 
-//     struct sockaddr_in their_addr; /* direccion IP y numero de puerto del cliente */
-//     /* addr_len contendra el taman~o de la estructura sockadd_in y numbytes el
-//     * numero de bytes recibidos */
-//     int addr_len, numbytes;
-//     int status;
-//     char buf[BUFFER_LEN]; /* Buffer de recepción */
-//     /* se crea el socket */
-
-//     if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
-//         perror("socket");
-//         exit(1);
-//     }
-
-//     /* Se establece la estructura my_addr para luego llamar a bind() */
-//     my_addr.sin_family      = AF_INET; /* usa host byte order */
-//     my_addr.sin_port        = htons(SERVER_PORT); /* usa network byte order */
-//     my_addr.sin_addr.s_addr = INADDR_ANY; /* escuchamos en todas las IPs */
-//     bzero(&(my_addr.sin_zero), 8); /* rellena con ceros el resto de la estructura */
-
-//     /* Se le da un nombre al socket (se lo asocia al puerto e IPs) */
-//     printf("Asignado direccion al socket ....\n");
-//     status = bind(sockfd, (struct sockaddr *)&my_addr, sizeof(struct sockaddr));
-    
-//     if (status == -1) {
-//         perror("bind");
-//         exit(2);
-//     }
-
-//     /* Se reciben los datos (directamente, UDP no necesita conexión) */
-//     addr_len = sizeof(struct sockaddr);
-//     printf("Esperando datos ....\n");
-//     numbytes = recvfrom(sockfd, buf, 
-//                         BUFFER_LEN, 0, 
-//                         (struct sockaddr *)&their_addr,
-//                         (socklen_t *)&addr_len);
-
-//     if ( numbytes == -1 ) {
-//         perror("recvfrom");
-//         exit(3);
-//     }
-//     /* Se visualiza lo recibido */
-//     printf("paquete proveniente de : %s\n",inet_ntoa(their_addr.sin_addr));
-//     printf("longitud del paquete en bytes: %d\n",numbytes);
-//     buf[numbytes] = '\0';
-//     printf("el paquete contiene: %s\n", buf);
-//     /* cerramos descriptor del socket */
-//     close(sockfd);
-//     exit (0);
-// }
